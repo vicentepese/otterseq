c7de326ab54002651996fcfbb724f3cf55620e71
vicentepese <sempereperisvicente@gmail.com>
2020-12-09 14:15:46 -0800

    Common SNPs across files

--- run_GWAS.py
+++ run_GWAS.py
@@ -17,6 +17,39 @@ def binarizeFiles(settings):
     # Call bash script to binarize files in Data/GWAS
     subprocess.call(['bash', settings['sh_script']['binarize.sh']])
 
+def get_SNP(settings, path):
+
+    # Get .bim files of controls
+    files = [file for file in os.listdir(path) if isfile(join(path, file)) and '.bim' in file]
+
+    # Initialize
+    totalSNPs = list()
+    SNPs = list()
+
+    # Geet SNPs
+    for file in files:
+        print("Parsing SNPs from " + file)
+        with open(join(path, file), 'r') as inFile:
+            for row in inFile:
+                row = row.split('\t')
+                SNPs.append(row[1])
+            totalSNPs.append(SNPs)
+    print('Finished parsing')
+
+    # Get intersection
+    commonSNPs = set(totalSNPs[0])
+    for i in range(1, len(totalSNPs)):
+        commonSNPs = commonSNPs.intersection(set(totalSNPs[i]))
+    commonSNPs = list(commonSNPs)
+
+    # Write common SNPS
+    with open(settings['file']['commonSNPs.txt'], 'w') as outFile:
+        for snp in commonSNPs:
+            outFile.write(snp + '\n')
+
+    return(commonSNPs)
+
+
 def main():
 
     # Open settings
@@ -32,7 +65,11 @@ def main():
     else:
         print("Files already binarized")
 
-    # 
+    # Get list of common SNPs across files 
+    commonSNPs = get_SNP(settings, path = settings['directory']['GWAS_binaries'])
+
+    # Merge files
+    mergeFiles
 
 
     
