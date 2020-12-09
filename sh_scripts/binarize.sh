8d4d155bb6be19c99bab55bf6c44f2ee013c3cee
vicentepese <sempereperisvicente@gmail.com>
2020-12-09 10:55:09 -0800

    Binarize final version

--- sh_scripts/binarize.sh
+++ sh_scripts/binarize.sh
@@ -1,9 +1,26 @@
 #!/bin/bash 
 
 # Get directory and files
+MAINDIR=$(pwd)
 GWASDIR=$(jq -r '.directory.GWAS' settings.json)
 GWASBINDIR=$(jq -r '.directory.GWAS_binaries' settings.json)
-GWASFILES=$(ls $GWASDIR)
+
+# Check if different datasets
+DBCOUNT=$(ls -l $GWASDIR | grep -c ^d)
+
+# If subdatasets in GWAS folder, create directories in GWAS_binaries
+if [[ $DBCOUNT -gt 1 ]] ; then 
+
+    # Go to directory, get list of datasets
+    cd $GWASDIR
+    DBLIST=$(ls */ -d)
+    cd $MAINDIR
+
+    # Creat directories
+    for DB in ${DBLIST[@]} ; do 
+        [[ -d $GWASBINDIR/$DB ]] || mkdir $GWASBINDIR/$DB
+    done 
+fi 
 
 # For each file, binarize
 for file in ${GWASFILES[@]} ; do
