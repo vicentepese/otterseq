2a6cad53760b057bc1104058bf522279478981a3
vicentepese <sempereperisvicente@gmail.com>
2021-02-04 12:03:58 -0800

    functional version

--- utils/HLA_imputation_ethnicity.R
+++ utils/HLA_imputation_ethnicity.R
@@ -19,19 +19,51 @@ setwd("~/Documents/GWAS_pipeline")
 # Import settings
 settings <- jsonlite::fromJSON('settings.json')
 
+######### IMPUTATION FUNCTION ############
+
+imputeHLA = function(data, model.list, eth){
+  
+  # Get loci
+  hla.id <- names(model.list)
+  
+  # Initialize output
+  pred.guess.out <- data.frame(sample.id = data$sample.id)
+  probs.out <- data.frame(sample.id = data$sample.id)
+  
+  # Impute for each locus
+  for (locus in hla.id){
+    
+    # Get model
+    model.hla <- hlaModelFromObj(model.list[[locus]])
+    summary(model.hla)
+    
+    # Make predictions
+    pred.guess <- predict(model.hla, data, type="response+prob", nclassifiers=100, cl=cl, match.type="Position")
+    preds.all <- pred.guess$value[,c("sample.id", "allele1", "allele2")]
+    colnames(preds.all) <- c("sample.id", paste0(locus, '.1'), paste0(locus, '.2'))
+    
+    # Get probabilities
+    probs <- pred.guess$value[,c("sample.id","prob")]
+    colnames(probs) <- c("sample.id", paste0("prob.", locus))
+    
+    # Merge 
+    pred.guess.out <- pred.guess.out %>% merge(preds.all, by = "sample.id")
+    probs.out <- probs.out %>% merge(probs, by = "sample.id")
+    
+  }
+  
+  # Add FID and IID
+  pred.guess.out$FID <- pred.guess.out$sample.id %>% lapply(function(x) strsplit(x,"-") %>% unlist() %>% .[1]) %>% unlist()
+  pred.guess.out$IID <-pred.guess.out$sample.id %>% lapply(function(x) strsplit(x,"-") %>% unlist() %>% tail(n=1)) %>% unlist()
+  probs.out$FID <- probs.out$sample.id %>% lapply(function(x) strsplit(x,"-") %>% unlist() %>% .[1]) %>% unlist()
+  probs.out$IID <-probs.out$sample.id %>% lapply(function(x) strsplit(x,"-") %>% unlist() %>% tail(n=1)) %>% unlist()
+  
+  # Write 
+  save(pred.guess.out, file = paste(settings$directory$HLA_Imputation, paste('HLA_eth', eth, sep = '_'), '.RData', sep= ''))
+  save(probs.out, file = paste(settings$directory$HLA_Imputation, paste('probs_eth', eth, sep = '_'), '.RData', sep= ''))
+}
+
 ########## HLA IMPUTATION ############
-
-# Impute HLA using individual models if there is an ethnicity file
-
-# Load pre-fit model and comvert to hlaMODEL
-model.list <- get(load(settings$file$PMRA_HLA_model))
-drb3 <- get(load(settings$file$PMRA_HLA_DRB3))
-drb4 <- get(load(settings$file$PMRA_HLA_DRB4))
-drb5 <- get(load(settings$file$PMRA_HLA_DRB5))
-model.list[["DRB3"]] <- drb3
-model.list[["DRB4"]] <- drb4
-model.list[["DRB5"]] <- drb5
-hla.id <- names(model.list)
 
 # Import file
 gname <- paste0(settings$plinkFiles$GWASQC, settings$plinkFiles$prefix, "_QC")
@@ -39,74 +71,103 @@ yourgeno <- hlaBED2Geno(bed.fn=paste(gname, ".bed", sep = ''), fam.fn=paste(gnam
                         , bim.fn=paste(gname, ".bim", sep=''), assembly = 'hg19')
 summary(yourgeno)
 
+# Import ethnicity
+eth.df <- read.table(file = settings$file$ethnicity, header = TRUE, sep = ",")
+eth.df$sample.id <- paste0(eth.df$FID, rep("-", nrow(eth.df)), eth.df$IID)
+
+# Load ethnicity models
+EUR.list <- get(load(settings$file$PMRA_EUR))
+AFR.list <- get(load(settings$file$PMRA_AFR))
+AS.list <- get(load(settings$file$PMRA_AS))
+AMR.list <- get(load(settings$file$PMRA_AMR))
+
 # Make cluster 
 cl <- makeCluster(10)
 
-# Make predictions
-for (locus in hla.id){
-  model.hla <- hlaModelFromObj(model.list[[locus]])
-  summary(model.hla)
-  pred.guess <- predict(model.hla, yourgeno, type="response+prob", nclassifiers=100, cl=cl, match.type="Position")
-  pred.guess$value$FID <- pred.guess$value$sample.id %>% lapply(function(x) strsplit(x,"-") %>% unlist() %>% .[1]) %>% unlist()
-  pred.guess$value$IID <- pred.guess$value$sample.id %>% lapply(function(x) strsplit(x,"-") %>% unlist() %>% tail(n=1)) %>% unlist()
-  save(pred.guess, file = paste(settings$directory$HLA_Imputation, paste('HLA_', locus, sep = ''), '.RData', sep= ''))
+# Parse ethnicities
+ethnicities <- unique(eth.df$Population)
+
+# Make predictions for each ethnicity
+for (eth in ethnicities){
+  
+  # Parse IDS
+  eth_ids <- eth.df %>% filter(Population == eth) %>% .['sample.id'] %>% unlist()
+  
+  # Filter 
+  data_eth.list <- yourgeno
+  data_eth.list$sample.id <- data_eth.list$sample.id[which(data_eth.list$sample.id %in% eth_ids)]
+  data_eth.list$genotype <- data_eth.list$genotype[,which(data_eth.list$sample.id %in% eth_ids)]
+  
+  # Impute for each locus 
+  switch (eth,
+    "EUR" = imputeHLA(data = data_eth.list, model.list = EUR.list, eth = "EUR"),
+    "AFR" = imputeHLA(data = data_eth.list, model.list = AFR.list, eth = "AFR"),
+    "SAS" = imputeHLA(data = data_eth.list, model.list = AS.list, eth = "SAS"),
+    "EAS" = imputeHLA(data = data_eth.list, model.list = AS.list, eth = "EAS"),
+    "AMR" = imputeHLA(data = data_eth.list, model.list = AMR.list, eth = "AMR")
+  )
+  
 }
+
 
 ################ MERGE #############
 
 # Initialize loop# List of files to merge 
 file.names <- list.files(settings$directory$HLA_Imputation) 
-file.names <- file.names[which(grepl(x=file.names, pattern = '.RData'))]
+file.names <- file.names[which(grepl(x=file.names, pattern = 'eth'))]
+probs.file.names <- file.names[which(grepl(x = file.names, pattern = "probs"))]
+HLA.file.names <- file.names[which(grepl(x = file.names, pattern = "HLA"))]
 
-# First pass
-f <- file.names[1]
-load(paste0(settings$directory$HLA_Imputation, f))
-HLA.data <- pred.guess$value
+# Initialize HLA for loop
+HLA.df <- data.frame()
 
-# Get probs and alleles
-probs.data <- HLA.data[,c("FID","IID", "sample.id","prob")]
-HLA.data <- HLA.data[,c("FID","IID", "sample.id","allele1", "allele2")]
-colnames(HLA.data) <- c("FID","IID", "sample.id", paste0(pred.guess$locus, '.1'), paste0(pred.guess$locus, '.2'))
-colnames(probs.data) <- c("FID","IID", "sample.id", paste0("prob.", pred.guess$locus))
+# Merge HLA dataframes
+for (HLA.file in HLA.file.names){
+  
+  # Print 
+  print(paste0("Merging :", HLA.file))
+  
+  # Load file
+  load(paste0(settings$directory$HLA_Imputation, HLA.file))
+  
+  # Bind by row
+  HLA.df <- rbind(HLA.df, pred.guess.out)
+  
+}
 
-# Merge rest of files
-for (f in file.names[2:length(file.names)]){
+# Initilize probabilities for loop
+probs.df <- data.frame()
+for (probs.file in probs.file.names){
   
-  # Print for loop
-  print(paste0("Current file: ", f))
+  # Print 
+  print(paste0("Merging :", probs.file))
   
-  # Load data and change colnames
-  load(paste0(settings$directory$HLA_Imputation, f))
-  data <- pred.guess$value[,c("FID","IID", "sample.id","allele1", "allele2")]
-  colnames(data) <- c("FID","IID", "sample.id", paste0(pred.guess$locus, '.1'), paste0(pred.guess$locus, '.2'))
+  # Load file 
+  load(paste0(settings$directory$HLA_Imputation, probs.file))
   
-  # Create probs and change colnames
-  probs <- pred.guess$value[,c("FID","IID", "sample.id","prob")]
-  colnames(probs) <- c("FID","IID", "sample.id", paste0("prob.", pred.guess$locus))
+  # Bind by row 
+  probs.df <- rbind(probs.df, probs.out)
   
-  # Merge by sample.id
-  HLA.data <- merge(HLA.data, data, by =  c("FID", "IID", "sample.id"))
-  
-  # Merge probs by sample.id
-  probs.data <- merge(probs.data, probs, by = c("FID", "IID", "sample.id"))
 }
 
 # Write 
-write.table(HLA.data, file = paste0(settings$directory$HLA_Imputation, "HLA_calls.csv"), sep = ',', quote = FALSE, row.names = FALSE, col.names = TRUE)
-write.table(probs.data, file = paste0(settings$directory$HLA_Imputation, "HLA_probs.csv"), sep = ',', quote = FALSE, row.names = FALSE, col.names = TRUE)
+write.table(HLA.df, file = paste0(settings$directory$HLA_Imputation, "HLA_calls_eth.csv"),
+            sep = ',', quote = FALSE, row.names = FALSE, col.names = TRUE)
+write.table(probs.df, file = paste0(settings$directory$HLA_Imputation, "HLA_probs_eth.csv"),
+            sep = ',', quote = FALSE, row.names = FALSE, col.names = TRUE)
 
 ############## PLOT PROBABILITIES #############
 
 ## Plot probabilities distributions
-pl <- vector('list', ncol(probs.data)-3)
+pl <- vector('list', ncol(probs.df)-3)
 idx <- 1
-for (i in 4:ncol(probs.data)){
+for (i in 2:(ncol(probs.df)-2)){
   
-  pl[[i-3]] <- local({
+  pl[[i-1]] <- local({
     i <- i
-    p1 <- ggplot(probs.data, aes(get(colnames(probs.data)[i]))) +
+    p1 <- ggplot(probs.df, aes(get(colnames(probs.df)[i]))) +
       geom_histogram() +
-      xlab(colnames(probs.data)[i]) + xlim(0,1)
+      xlab(colnames(probs.df)[i]) + xlim(0,1)
     print(p1)
   })
 }
@@ -118,21 +179,22 @@ pca <- read.table(file = settings$file$PCA_eigenvec, sep = " ", header = FALSE)
 colnames(pca) <- c("FID", "IID", paste0(rep("PC", 20), as.character(1:20)))
 pca$sample.id <- paste0(pca$FID, rep('-', nrow(pca)), pca$IID)
 
-# Merge PCA with probs 
-data_plot <- merge(pca, probs.data, by = c("FID", "IID","sample.id"))
+# Merge PCA with probs and ethnicity
+data_plot <- merge(pca, probs.df, by = c("sample.id"))
+data_plot <-merge(data_plot, eth.df, by = "sample.id")
 
 # Plot PCAs with probs
-pl <- vector('list', ncol(probs.data)-3)
+pl <- vector('list', ncol(probs.df)-3)
 idx <- 1
-for (i in 4:ncol(probs.data)){
+for (i in 2:(ncol(probs.df)-2)){
   
-  pl[[i-3]] <- local({
+  pl[[i-1]] <- local({
     i <- i
-    p1 <- ggplot(data_plot, aes(PC1, PC2, color = get(colnames(probs.data)[i]))) + 
-      geom_point() +  
+    p1 <- ggplot(data_plot, aes(PC1, PC2, color = get(colnames(probs.df)[i]))) + 
+      geom_point(aes(shape = Population)) +  
       scale_colour_gradientn(limits = c(0,1), colors =c("navyblue", "darkmagenta", "darkorange1"), oob = scales::squish) + 
-      labs(color = colnames(probs.data)[i])
-    print(p1)
+      labs(color = colnames(probs.df)[i])
+    ggplotly(p1)
   })
 }
 do.call(grid.arrange, pl)
