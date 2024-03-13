e3ca29ead3852213fa79ac64ccd8c91fe14c34ef
Vicente Peris Sempere <46353112+vicentepese@users.noreply.github.com>
2024-03-13 18:29:24 +0100

    Update pca.sh
    Update pca to log in terminal and use a file instead of passing snps in list

--- sh_scripts/pca.sh
+++ sh_scripts/pca.sh
@@ -20,13 +20,12 @@ PREFIX=$(jq -r '.plinkFiles.prefix' settings.json)
 
 # Parse SNPs (rs_xxxxxxxx) belonging to HLA region (EXCLUDE VARS with ,?)
 ./bin/plink --bfile ${GWASQC}${PREFIX}_QC --allow-no-sex --chr 6 --from-bp 28477797 --to-bp 33448354 --make-bed --out temp > temp
-rsExclude=$(awk '{ ORS=", "};{print $2}' temp.bim)
+awk '{print $2}' temp.bim > exclude.txt
 rm -r temp*
 
 # Compute PCA excluding by IBD, missingness, and HLA region
 ./bin/plink --bfile ${GWASQC}${PREFIX}_QC \
     --allow-no-sex \
-    --exclude-snps $rsExclude --d ';' \
-    --pca 20  --out $PCA > $PCA
-
-
+    --exclude exclude.txt \
+    --pca 20  --out $PCA
+rm exclude.txt
